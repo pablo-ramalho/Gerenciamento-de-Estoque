2453fefa7949d25b08af780cf9a57b52f54a47b3
pablo-ramalho <pabloamaral512@gmail.com>
2026-03-08 20:04:42 -0300

    Removida coleção de ArrayList<Produto> (a lista será criada na classe Gui)

--- MainClass.java
+++ MainClass.java
@@ -1,15 +1,11 @@
 import Elements.Produto;
 import Elements.Gui;
-import java.util.Deque;
-import java.util.LinkedList;
 
 public class MainClass{
 
     public static void main(String[] args){
         Gui janela;
-        Deque<Produto> produtos;
 
-        produtos = new LinkedList<>();
         janela = new Gui();
 
 
